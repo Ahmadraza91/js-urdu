ed820bb9709e7f13d20a2647ee9e036ba66e977c
Ahmadraza91 <ahmadrazaahsan7@gmail.com>
2026-07-04 06:17:47 +0000

    datatypes

---
+++ 01_basics/02datatypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as a newer version
+
+//alert(3+3) // we are using nodejs not browser
+let name="Ahmad"
+let age=22
+let isloggedIn=false
+
+//premitive datatypes..........
+
+// number=> 2 power 53
+// bigint
+//string=> ""
+// boolean=>true/false
+//null=> standalone value representation of empty value datatype object
+// undefined =>datatype undefined
+//symbol=>unique 
+
+// object
+
+console.log(typeof null)
