6481e686d9e21c5897fde389141186c590d7e61f
Ahmadraza91 <ahmadrazaahsan7@gmail.com>
2026-07-04 05:47:44 +0000

    container config done

---
+++ 01_basics/variables.js
@@ -0,0 +1,21 @@
+const accountId =144553
+let accountEmail="abc@gmail.com"
+var password=917181
+accountCity="wazirabad"
+let accountState
+
+//accountId=4433 //not allowed
+/*
+prefer not to use var because of block and global space acess issue
+functional scope
+
+*/
+
+accountEmail="bdf@gmail.com"
+
+password=88888
+
+accountCity="grw"
+
+
+console.table([accountId,accountEmail,accountCity,password,accountState])
