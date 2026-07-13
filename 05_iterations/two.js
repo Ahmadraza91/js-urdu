af6e40ec4fb24d3d6003ab33b1fb1db4ae4bc419
Ahmadraza91 <ahmadrazaahsan7@gmail.com>
2026-07-13 15:38:24 +0000

    while and do while loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,34 @@
+// while
+let i=0;
+while(i<=10) {
+
+    //console.log(`value of i is: ${i}`);
+    i+=2;
+    
+    
+}
+
+const myarr=["flash","buthcer","starlight"]
+let arr=0;
+while(arr<myarr.length){
+  //  console.log(myarr[arr]);
+    arr++;
+    
+
+}
+
+// do while
+
+let score =1
+
+do{
+    //console.log(` score is ${score}`);
+    score++;
+}while(score<=10)
+
+    const myarrr=["ahmad"]
+    let ind=0
+    do{
+        console.log(`value is ${myarrr[ind]}`)
+        ind++
+    }while(ind<myarrr.length)
